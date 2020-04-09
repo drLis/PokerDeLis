238d177141c3730873d2e923101d5442be7e2aeb
dr.lis <dr.lis@outlook.com>
2020-04-09 23:40:57 +0300

    Card deck and hands for poker sic plus completed

---
+++ contracts/CardDeck.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.5;
+
+enum Suit
+{
+	Clubs,
+	Diamonds,
+	Hearts,
+	Spades
+}
+
+enum RankSixPlus
+{
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+	Ace
+}
---
+++ contracts/PokerHands.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.5;
+
+enum PokerHands
+{
+	HighCard,
+	OnePair,
+	TwoPair,
+	ThreeOfAKind,
+	Straight,
+	FullHouse,
+	Flush,
+	FoureOfAKind,
+	StraightFlush
+}
