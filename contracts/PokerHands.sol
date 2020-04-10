f4efce6d2da3932a1fea8dae405ae863a6c3d5b5
dr.lis <dr.lis@outlook.com>
2020-04-10 22:43:46 +0300

    Sorts added

--- contracts/PokerHands.sol
+++ contracts/PokerHands.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.6.5;
 
+import "./CardDeck.sol";
+
 enum PokerHands
 {
 	HighCard,
@@ -11,4 +13,18 @@ enum PokerHands
 	Flush,
 	FourOfAKind,
 	StraightFlush
+}
+
+library PokerHandsValidator
+{
+	function sortFiveCardsByRank(Card[5] memory cards) internal pure returns (Card[5] memory)
+	{
+		Card[5] memory new_card;
+		
+	}
+
+	function sortSevenCardsByRank(Card[7] memory cards) internal pure returns (Card[7] memory)
+	{
+
+	}
 }
